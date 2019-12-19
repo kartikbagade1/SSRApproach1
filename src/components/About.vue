35c7b80da538caff2cc0dbd6a786a5845ce1d47a
Kartik <kartik.bagade@mindstix.com>
2019-12-19 16:07:33 +0530

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        {{ aboutText }}
+    </div>
+</template>
+<script>export default {
+  data () {
+    return {
+      aboutText: 'About Component'
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        {{ aboutText }}
+    </div>
+</template>
+<script>export default {
+  data () {
+    return {
+      aboutText: 'Home Component'
+    }
+  }
+}
+</script>
